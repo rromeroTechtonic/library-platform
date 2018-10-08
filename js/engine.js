f75a2ee1f8184f97278af3513256c25f5c3238cf
Renee Romero <>
2018-10-08 08:35:34 -0600

    Most updated assignment

--- js/engine.js
+++ js/engine.js
@@ -5,41 +5,50 @@ function Library(){
 Library.prototype.addBook = function(book)
 {
   for(var i=0; i<this.bookShelf.length; i++) {
-      if(this.bookShelf[i].title === book.title){
+      if(this.bookShelf[i].title.toLowerCase().trim() === book.title.toLowerCase().trim()){
         return false;
       }
     }
     this.bookShelf.push(book);
     return true;
+ //
 };
 
-Library.prototype.removeBookByTitle = function(title)
+Library.prototype.removeBookByTitle = function(title)//title will be a string
  {
   for(var i=0; i<this.bookShelf.length; i++) {
-     if(this.bookShelf[i].title === book.title){
-     return false;
+    //1st time we loop this.bookShelf[0].title value is "harry potter"
+    //2nd time we loop this.bookShelf[1].title value is "IT"
+     if(this.bookShelf[i].title === title){
+        //only way to end up inside this if statement is if the value of this.bookShelf[i].title equals title
+      this.bookShelf.splice(i,1)
+      return true;
        }
      }
-     this.bookShelf.splice(title);
-     return true;
+return false;
 };
 
 Library.prototype.removeBookByAuthor = function(author)
   {
+    var haveRemoved = false;
     for(var i=0; i<this.bookShelf.length; i++) {
       if(this.bookShelf[i].author === author){
-        this.bookShelf.splice(i,1,);
+        this.bookShelf.splice(i,1);
         i--;
-        return true;
+        haveRemoved = true;
       }
     }
-    return false;
+    return haveRemoved;
 };
 
 Library.prototype.getRandomBook = function()
 {
-  max = Library.length;
-  var getRandomBook = Library[Math.floor(Math.random() * max)];
+  if(this.bookShelf.length === 0){
+    return null;
+  }
+
+  var max = this.bookShelf.length;
+  var getRandomBook = this.bookShelf[Math.floor(Math.random() * max)];
   return getRandomBook;
 };
 
